35cdf4beea8f82fe2ebda04ed8519c312a128959
susandahal <susandahal53@gmail.com>
2025-08-05 20:01:47 +0545

    sweet:Init YAAP

--- yaap_sweet.mk
+++ yaap_sweet.mk
@@ -13,9 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := yaap_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
